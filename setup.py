6b4bdc5f5933dcfcf4060d5de7e1394d9da77154
Stuart Mumford <stuart@cadair.com>
2020-01-06 10:18:53 +0000

    Use pyproject to ensure numpy and extra requires instead of requirements

--- setup.py
+++ setup.py
@@ -1,13 +1,8 @@
 import os
 import setuptools
 
-# Need to install numpy first
-import subprocess
-import sys
-subprocess.check_call([sys.executable, "-m", "pip", "install", "numpy"])
-
-
 from numpy.distutils.core import setup, Extension
+
 # Don't want to build the fortran on readthedocs
 exts = []
 if not os.environ.get('READTHEDOCS', None):
@@ -18,16 +13,4 @@ if not os.environ.get('READTHEDOCS', None):
                        ),
              ]
 
-if __name__ == "__main__":
-    setup(name='streamtracer',
-          version='0.1.2',
-          description='Python wrapped fortran to calculate streamlines',
-          url='https://github.com/dstansby/streamtracer',
-          author='David Stansby, Lars Mejnertsen',
-          author_email='dstansby@gmail.com',
-          install_requires=['numpy'],
-          python_requires='>=3.6',
-          packages=['streamtracer'],
-          ext_modules=exts,
-          include_package_data=True,
-          )
+setup(ext_modules=exts)
