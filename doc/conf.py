8aabed5d839e330f7058d8a6a555d1c401c5d666
Stuart Mumford <stuart@cadair.com>
2020-01-06 10:28:51 +0000

    Swap to napoleon over numpydoc

--- doc/conf.py
+++ doc/conf.py
@@ -39,9 +39,10 @@ master_doc = 'index'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx_automodapi.automodapi',
-              'numpydoc',
+              'sphinx.ext.napoleon',
               ]
-numpydoc_show_class_members = False
+# Disable google style docstrings
+napoleon_google_docstring = False
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
