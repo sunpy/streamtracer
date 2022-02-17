8277c2a59f6ceb048dbe5d8e8e9b3a6210d0ce38
David Stansby <dstansby@gmail.com>
2022-02-17 12:16:03 +0000

    Fix indentation

--- streamtracer/fortran/Streamtracer.f90
+++ streamtracer/fortran/Streamtracer.f90
@@ -1,63 +1,63 @@
-    module streamtracer
-    use omp_lib
-    implicit none
+module streamtracer
+use omp_lib
+implicit none
 
-    ! Connectivity tracer!
+! Connectivity tracer!
 
-    ! ROT: Reason of termination
-    ! If  ROT = 0: Still running
-    !     ROT = 1: Out of steps
-    !     ROT = 2: Out of domain
-    !     ROT = -1: vmag = 0
-    !     ROT = -2: NaN present
+! ROT: Reason of termination
+! If  ROT = 0: Still running
+!     ROT = 1: Out of steps
+!     ROT = 2: Out of domain
+!     ROT = -1: vmag = 0
+!     ROT = -2: NaN present
 
-    integer :: ns, openmp_enabled = 0, debug = 0
-    double precision :: ds
-    double precision, dimension(3) :: xc
+integer :: ns, openmp_enabled = 0, debug = 0
+double precision :: ds
+double precision, dimension(3) :: xc
 
-    contains
+contains
 
-    subroutine thread_count(message)
+  subroutine thread_count(message)
     character(len=*), intent(in) :: message
     integer :: num_threads, max_threads
 
-      num_threads = get_num_threads()
-      max_threads = get_max_threads()
+    num_threads = get_num_threads()
+    max_threads = get_max_threads()
 
-      !$ openmp_enabled = 1
+    !$ openmp_enabled = 1
 
-      !$omp single
+    !$omp single
       open(unit=500, file='threads.txt', access='append')
       write(500, 200) message, num_threads, max_threads
       close(500)
-200 format(A, ': ', I4, ', ', I4)
-      !$omp end single
+      200 format(A, ': ', I4, ', ', I4)
+    !$omp end single
 
-    end subroutine thread_count
-
-
-    subroutine set_num_threads(n_threads)
-      integer, intent(in) :: n_threads
-      !$ openmp_enabled = 1
-      call omp_set_num_threads(n_threads)
-    end subroutine set_num_threads
+  end subroutine thread_count
 
 
-    integer function get_num_threads()
-      !$ openmp_enabled = 1
-      !$omp parallel
-      get_num_threads = omp_get_num_threads()
-      !$omp end parallel
-    end function get_num_threads
+  subroutine set_num_threads(n_threads)
+    integer, intent(in) :: n_threads
+    !$ openmp_enabled = 1
+    call omp_set_num_threads(n_threads)
+  end subroutine set_num_threads
 
 
-    integer function get_max_threads()
-      !$ openmp_enabled = 1
-      get_max_threads = omp_get_max_threads()
-    end function get_max_threads
+  integer function get_num_threads()
+    !$ openmp_enabled = 1
+    !$omp parallel
+    get_num_threads = omp_get_num_threads()
+    !$omp end parallel
+  end function get_num_threads
 
 
-    subroutine streamline_array(x0, nlines, v, nx, ny, nz, d, dir, ns, cyclic, xs, ROT, ns_out)
+  integer function get_max_threads()
+    !$ openmp_enabled = 1
+    get_max_threads = omp_get_max_threads()
+  end function get_max_threads
+
+
+  subroutine streamline_array(x0, nlines, v, nx, ny, nz, d, dir, ns, cyclic, xs, ROT, ns_out)
     ! INPUT:
     ! x0: seed points
     ! v: vector field
@@ -82,40 +82,37 @@
     integer :: i, j
 
     !$ openmp_enabled = 1
-
     !$omp parallel default(firstprivate) shared(v, xs, x0, ROT, ns_out)
 
     if (debug > 0) call thread_count('streamline_array')
-
     !$omp do schedule(dynamic)
-        !DIR$ NOUNROLL
-    do i = 1, nlines
+      !DIR$ NOUNROLL
+      do i = 1, nlines
         x0_i = x0(i,:)
         call streamline(x0_i, v, nx, ny, nz, d, dir, ns, cyclic, xs_i, ROT(i), ns_out(i))
         do j = 1, ns_out(i)
-            xs(i, j,:) = xs_i(j,:)
+          xs(i, j,:) = xs_i(j,:)
         end do
-    end do
-  !$omp end do
-
+      end do
+    !$omp end do
     !$omp end parallel
 
-    end subroutine streamline_array
+  end subroutine streamline_array
 
-    subroutine streamline(x0, v, nx, ny, nz, d, dir, ns_in, cyclic, xs, ROT, ns_out)
-      ! INPUT:
-      ! x0: seed point
-      ! v: vector field
-      ! nx, ny, nz: grid spacing in (x, y, z) directions
-      ! d:
-      ! dir: direction to trace (1=forwards, -1=backwards)
-      ! ns_in: max steps
-      ! cyclic: if true, cyclic boundaries (instead of terminating)
-      !
-      ! OUTPUT:
-      ! xs: traced streamline
-      ! ROT: reason of termination
-      ! ns_out: number of steps taken
+  subroutine streamline(x0, v, nx, ny, nz, d, dir, ns_in, cyclic, xs, ROT, ns_out)
+    ! INPUT:
+    ! x0: seed point
+    ! v: vector field
+    ! nx, ny, nz: grid spacing in (x, y, z) directions
+    ! d:
+    ! dir: direction to trace (1=forwards, -1=backwards)
+    ! ns_in: max steps
+    ! cyclic: if true, cyclic boundaries (instead of terminating)
+    !
+    ! OUTPUT:
+    ! xs: traced streamline
+    ! ROT: reason of termination
+    ! ns_out: number of steps taken
     implicit none
     double precision, dimension(3), intent(in) :: x0, d
     double precision, dimension(nx, ny, nz, 3), intent(in) :: v
@@ -129,7 +126,6 @@
     !$ openmp_enabled = 1
 
     ns = ns_in
-
     ROT = 0
 
     ! Set all streamline points to (0, 0, 0) to start
@@ -139,15 +135,14 @@
     xi = x0
 
     do i = 2, ns
+      ! Do a single step
+      call RK4_update(xi, v, nx, ny, nz, d, dir)
+      ! Check if we are out of bounds, and move if cyclic is on
+      call check_bounds(xi, nx, ny, nz, d, cyclic, ROT)
+      ! Save the step value
+      xs(i,:) = xi
 
-        ! Do a single step
-        call RK4_update(xi, v, nx, ny, nz, d, dir)
-        ! Check if we are out of bounds, and move if cyclic is on
-        call check_bounds(xi, nx, ny, nz, d, cyclic, ROT)
-        ! Save the step value
-        xs(i,:) = xi
-
-        if (ROT /= 0) exit
+      if (ROT /= 0) exit
     end do
 
     ns_out = i
@@ -156,25 +151,20 @@
       ROT = 1
       ns_out = ns
     end if
-
-    end subroutine streamline
+  end subroutine streamline
 
 
   double precision function vector_dot(v1, v2)
     double precision, dimension(3), intent(in) :: v1, v2
-
-  vector_dot = v1(1) * v2(1) + v1(2) * v2(2) + v1(3) * v2(3)
-
+    vector_dot = v1(1) * v2(1) + v1(2) * v2(2) + v1(3) * v2(3)
   end function vector_dot
 
   double precision function vector_mag(v)
     double precision, dimension(3), intent(in) :: v
-
-  vector_mag = sqrt(vector_dot(v, v))
-
+    vector_mag = sqrt(vector_dot(v, v))
   end function vector_mag
 
-    subroutine RK4_update(xi, v, nx, ny, nz, d, dir)
+  subroutine RK4_update(xi, v, nx, ny, nz, d, dir)
     double precision, dimension(3), intent(inout) :: xi
     double precision, dimension(3), intent(in) :: d
     double precision, dimension(nx, ny, nz, 3), intent(in) :: v
@@ -202,9 +192,9 @@
     !DIR$ NOUNROLL
     xi = xi + (k1 + 2 * k2 + 2 * k3 + k4) / 6.
 
-    end subroutine RK4_update
+  end subroutine RK4_update
 
-    subroutine check_bounds(xi, nx, ny, nz, d, cyclic, ROT)
+  subroutine check_bounds(xi, nx, ny, nz, d, cyclic, ROT)
     ! INPUT
     ! xi: vector position
     ! nx, ny, nz: number of grid points in (x, y, z)
@@ -219,15 +209,15 @@
 
     ROT = 0
     if ( isnan(xi(1)) .or. isnan(xi(2)) .or. isnan(xi(3)) ) then
-        ! Encountered a NaN
-        ROT = - 2
+      ! Encountered a NaN
+      ROT = - 2
     elseif (xi(1) < 0 .or. xi(1) > d(1) * (nx - 1)) then
-        if (cyclic(1) /= 0) then
-          xi(1) = MOD(xi(1) + d(1) * (nx - 1), d(1) * (nx - 1))
-        else
-          ! Out of bounds
-          ROT = 2
-        end if
+      if (cyclic(1) /= 0) then
+        xi(1) = MOD(xi(1) + d(1) * (nx - 1), d(1) * (nx - 1))
+      else
+        ! Out of bounds
+        ROT = 2
+      end if
     elseif (xi(2) < 0 .or. xi(2) > d(2) * (ny - 1)) then
       if (cyclic(2) /= 0) then
         xi(2) = MOD(xi(2) + d(2) * (ny - 1), d(2) * (ny - 1))
@@ -244,9 +234,9 @@
       end if
     end if
 
-    end subroutine check_bounds
+  end subroutine check_bounds
 
-    subroutine stream_function(xI, v, nx, ny, nz, d, dir, f)
+  subroutine stream_function(xI, v, nx, ny, nz, d, dir, f)
     implicit none
     double precision, dimension(3), intent(in) :: xI
     double precision, dimension(nx, ny, nz, 3), intent(in) :: v
@@ -298,12 +288,12 @@
     ! vI(3))
 
     !DIR$ NOUNROLL
-  vmag  = sqrt(vI(1) ** 2 + vI(2) ** 2 + vI(3) ** 2)
+    vmag  = sqrt(vI(1) ** 2 + vI(2) ** 2 + vI(3) ** 2)
     f = dir * vI / vmag * ds
 
-    end subroutine stream_function
+  end subroutine stream_function
 
-    subroutine interpolate(xI, v, nx, ny, nz, d, vI)
+  subroutine interpolate(xI, v, nx, ny, nz, d, vI)
     implicit none
     double precision, dimension(3), intent(in) :: xI
     double precision, dimension(nx, ny, nz, 3), intent(in) :: v
@@ -334,11 +324,10 @@
     cell = v(i0(1):i1(1), i0(2):i1(2), i0(3):i1(3), 3)
     call interp_trilinear(distI, cell, vI(3))
 
-    end subroutine interpolate
+  end subroutine interpolate
 
-    ! --- Trilinear interpolation function -------------------------------------------------------------
 
-    subroutine interp_trilinear(xd, f, fI)
+  subroutine interp_trilinear(xd, f, fI)
     implicit none
     double precision, intent(in), dimension(3) :: xd
     double precision, intent(in), dimension(0:1, 0:1, 0:1) :: f
@@ -366,9 +355,9 @@
 
     fI = c0 * m_xd(3) + c1 * xd(3)
 
-    end subroutine interp_trilinear
+  end subroutine interp_trilinear
 
-    subroutine interp_trilinear2(xd, f000, f001, f010, f011, f100, f101, f110, f111, fI)
+  subroutine interp_trilinear2(xd, f000, f001, f010, f011, f100, f101, f110, f111, fI)
     implicit none
     double precision, intent(in), dimension(3) :: xd
     double precision, intent(in) :: f000, f001, f010, f011, f100, f101, f110, f111
@@ -395,6 +384,6 @@
 
     fI = c0 * m_xd(3) + c1 * xd(3)
 
-    end subroutine interp_trilinear2
+  end subroutine interp_trilinear2
 
-    end module streamtracer
+end module streamtracer
