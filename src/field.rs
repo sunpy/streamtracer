35acf14d5c5001b9252c688b7390b136985603c9
David Stansby <dstansby@gmail.com>
2022-09-26 16:43:17 +0100

    Add grid_idx method

--- src/field.rs
+++ src/field.rs
@@ -1,5 +1,5 @@
 /// Structure for holding a 3D vector field
-use ndarray::{Array1, Array4};
+use ndarray::{array, Array1, Array4};
 
 
 pub struct VectorField<'a> {
@@ -8,9 +8,13 @@ pub struct VectorField<'a> {
     zgrid: &'a Array1<f64>,
     field: &'a Array4<f64>,
     cyclic: &'a Array1<bool>,
+    nx: usize,
+    ny: usize,
+    nz: usize
 }
 
 impl<'a> VectorField<'a> {
+    // Create a new VectorField, checking for appropriate array shapes
     pub fn new(
         xgrid: &'a Array1<f64>,
         ygrid: &'a Array1<f64>,
@@ -31,6 +35,39 @@ impl<'a> VectorField<'a> {
 
         assert_eq!(cyclic.shape()[0], 3);
 
-        Self{xgrid, ygrid, zgrid, field, cyclic}
+        Self{xgrid, ygrid, zgrid, field, cyclic, nx, ny, nz}
+    }
+
+    // Return grid index of the cell containing `x`.
+    pub fn grid_idx(
+        &self,
+        x: &Array1<f64>
+    ) -> Array1<usize>{
+
+        let mut grid_idx = array![self.nx, self.ny, self.nz];
+
+        // x
+        for i in 0..self.nx{
+            if x[0] >= self.xgrid[[i]] && x[0] < self.xgrid[[i+1]]{
+                grid_idx[0] = i;
+                break;
+            }
+        }
+        // y
+        for i in 0..self.ny{
+            if x[1] >= self.ygrid[[i]] && x[1] < self.ygrid[[i+1]]{
+                grid_idx[1] = i;
+                break;
+            }
+        }
+        // z
+        for i in 0..self.nz{
+            if x[2] >= self.zgrid[[i]] && x[2] < self.zgrid[[i+1]]{
+                grid_idx[2] = i;
+                break;
+            }
+        }
+
+        return grid_idx
     }
 }
--- tests/test_field.rs
+++ tests/test_field.rs
@@ -7,10 +7,20 @@ mod tests{
     fn test_field() {
         let xcoords = array![0.1, 0.2, 0.3];
         let ycoords = array![1.0, 1.1, 1.2, 1.3];
-        let zcoords = array![-0.1, -0.2];
-        let field = Array::zeros((3, 4, 2, 3));
+        let zcoords = array![-0.1, 0.0, 1.0, 50.0, 56.0];
+        let field = Array::zeros((3, 4, 5, 3));
         let cyclic = array![true, false, true];
 
-        let _f = VectorField::new(&xcoords, &ycoords, &zcoords, &field, &cyclic);
+        let f = VectorField::new(&xcoords, &ycoords, &zcoords, &field, &cyclic);
+
+        // Test grid_idx
+        let x = array![0.15, 1.05, -0.05];
+        assert_eq!(f.grid_idx(&x), array![0, 0, 0]);
+
+        let x = array![0.25, 1.05, -0.05];
+        assert_eq!(f.grid_idx(&x), array![1, 0, 0]);
+
+        let x = array![0.25, 1.15, 53.0];
+        assert_eq!(f.grid_idx(&x), array![1, 1, 3]);
     }
 }
