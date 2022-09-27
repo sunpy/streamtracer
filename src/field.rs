b93c5d37b2db94112c2af9a1b85ecf8a6fd676fb
David Stansby <dstansby@gmail.com>
2022-09-27 15:27:37 +0100

    Add docs to field and interp

--- src/field.rs
+++ src/field.rs
@@ -1,32 +1,47 @@
-/// Structure for holding a 3D vector field
-use ndarray::{array, Array1, Array4};
+//! Structure for representing a 3D vector field defined on the corners
+//! of a rectilinear grid.
 
+use ndarray::{array, Array1, Array4, s};
 
+use crate::interp::interp_trilinear;
+
+/// A 3D vector field defined at grid corners.
 pub struct VectorField<'a> {
-    xgrid: &'a Array1<f64>,
-    ygrid: &'a Array1<f64>,
-    zgrid: &'a Array1<f64>,
-    field: &'a Array4<f64>,
+    /// Grid points along x dimension.
+    pub xgrid: &'a Array1<f64>,
+    /// Grid points along y dimension.
+    pub ygrid: &'a Array1<f64>,
+    /// Grid points along z dimension.
+    pub zgrid: &'a Array1<f64>,
+    /// Vector values at each grid point. Must be shape
+    /// (nx, ny, ny, 3), where (nx, ny, nz) are the number
+    /// of coordinates along dimension.
+    pub values: &'a Array4<f64>,
+    /// Whether each dimension should be treated as cyclic
+    /// or not. Must be shape (3,).
     cyclic: &'a Array1<bool>,
+    /// Number of x coordinates.
     nx: usize,
+    /// Number of y coordinates.
     ny: usize,
+    /// Number of z coordinates.
     nz: usize
 }
 
 impl<'a> VectorField<'a> {
-    // Create a new VectorField, checking for appropriate array shapes
+    /// Create a new VectorField, checking for appropriate array shapes.
     pub fn new(
         xgrid: &'a Array1<f64>,
         ygrid: &'a Array1<f64>,
         zgrid: &'a Array1<f64>,
-        field: &'a Array4<f64>,
+        values: &'a Array4<f64>,
         cyclic: &'a Array1<bool>
     ) -> Self{
         // Do some shape checking
         let nx = xgrid.shape()[0];
         let ny = ygrid.shape()[0];
         let nz = zgrid.shape()[0];
-        let field_shape = field.shape();
+        let field_shape = values.shape();
 
         assert_eq!(field_shape[0], nx);
         assert_eq!(field_shape[1], ny);
@@ -35,10 +50,10 @@ impl<'a> VectorField<'a> {
 
         assert_eq!(cyclic.shape()[0], 3);
 
-        Self{xgrid, ygrid, zgrid, field, cyclic, nx, ny, nz}
+        Self{xgrid, ygrid, zgrid, values, cyclic, nx, ny, nz}
     }
 
-    // Return grid index of the cell containing `x`.
+    /// Return grid index of the cell containing `x`.
     pub fn grid_idx(
         &self,
         x: &Array1<f64>
@@ -69,5 +84,45 @@ impl<'a> VectorField<'a> {
         }
 
         return grid_idx
+    }
+
+    /// Get vector at position `x` using tri-linear interpolation.
+    pub fn vector_at_position(
+        &self,
+        x0: &Array1<f64>
+    ) -> Array1<f64>{
+        let cell_idx = self.grid_idx(x0);
+        let cell_origin = array![
+            self.xgrid[cell_idx[0]],
+            self.ygrid[cell_idx[1]],
+            self.zgrid[cell_idx[2]]
+        ];
+        let cell_size = array![
+            self.xgrid[cell_idx[0]+1] - cell_origin[0],
+            self.ygrid[cell_idx[1]+1] - cell_origin[1],
+            self.zgrid[cell_idx[2]+1] - cell_origin[2]
+        ];
+
+        // Distance along each cell edge in normalised units
+        let cell_dist: Array1<f64> = (x0 - cell_origin) / cell_size;
+
+        // Eight corners of the cube that the position vector is
+        // currently in
+        let vec_cube = self.values.slice(s![
+            cell_idx[0]..(cell_idx[0] + 1),
+            cell_idx[1]..(cell_idx[1] + 1),
+            cell_idx[2]..(cell_idx[3] + 1),
+            ..
+        ]);
+
+
+        let mut vector_at_pos = array![0., 0., 0.];
+        // Loop over vector components
+        for i in 0..3{
+            vector_at_pos[[i]] = interp_trilinear(
+                &vec_cube.slice(s![.., .., .., i]),
+                &cell_dist);
+        }
+        return vector_at_pos;
     }
 }
--- src/interp.rs
+++ src/interp.rs
@@ -1,13 +1,22 @@
-use ndarray::{Array3, Array1, Array};
+//! Helper functions for interpolation.
+
+use ndarray::{Array1, Array, ArrayBase, Ix3, Data};
+
 
 /// Trilinear-interpolation of a scalar defined on
 /// the eight corners of a cuboid.
 ///
 /// # Arguments
 ///
-/// * `values` - Values on the eight cube corners
-/// * `x` - Coordinate to interpolate at. Components must be >= 0 and <=1
-pub fn interp_trilinear(values: &Array3<f64>, x: &Array1<f64>) -> f64 {
+/// * `values` - Values on the eight cube corners. Must be shape `(2, 2, 2)`.
+/// * `x` - Coordinate to interpolate at. Components must be `>= 0` and `<=1`. Must be shape `(3,)`.
+pub fn interp_trilinear<S>(
+    values: &ArrayBase<S, Ix3>,
+    x: &Array1<f64>
+) -> f64
+where
+    S: Data<Elem=f64>
+{
     let m_x = 1. - x;
 
     let mut c = Array::zeros((2, 2));
