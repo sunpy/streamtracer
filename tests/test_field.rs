f32a836876ba5b1080852c9030b5c4ba3cbbe308
David Stansby <dstansby@gmail.com>
2022-09-30 12:37:22 +0100

    Add cyclic stuff to field

--- tests/test_field.rs
+++ tests/test_field.rs
@@ -9,9 +9,9 @@ mod tests{
     /// Get a simple vector field
     /// This implicitly tests `new()`.
     fn get_field() -> VectorField {
-        let xcoords = array![0.1, 0.2, 0.3];
-        let ycoords = array![1.0, 1.1, 1.2, 1.3];
-        let zcoords = array![-0.1, 0.0, 1.0, 50.0, 56.0];
+        let xcoords = array![0., 0.2, 0.3];
+        let ycoords = array![0., 1.1, 1.2, 1.3];
+        let zcoords = array![0.0, 1.0, 50.0, 56.0, 100.0];
         let mut field = Array::zeros((3, 4, 5, 3));
 
         field.slice_mut(s![2, 2.., 2.., 0]).fill(1.);
@@ -24,21 +24,21 @@ mod tests{
     fn test_grid_idx() {
         let f = get_field();
 
-        let x = array![0.15, 1.05, -0.05];
+        let x = array![0.15, 1.05, 0.05];
         assert_eq!(f.grid_idx(x.view()), array![0, 0, 0]);
 
-        let x = array![0.25, 1.05, -0.05];
+        let x = array![0.25, 1.05, 0.05];
         assert_eq!(f.grid_idx(x.view()), array![1, 0, 0]);
 
         let x = array![0.25, 1.15, 53.0];
-        assert_eq!(f.grid_idx(x.view()), array![1, 1, 3]);
+        assert_eq!(f.grid_idx(x.view()), array![1, 1, 2]);
     }
 
     #[test]
     fn test_vector_at_position() {
         let f = get_field();
 
-        let mut x = array![0.15, 1.05, -0.05];
+        let mut x = array![0.15, 1.05, 0.05];
 
         assert_eq!(f.vector_at_position(x.view()), array![0., 0., 0.]);
 
